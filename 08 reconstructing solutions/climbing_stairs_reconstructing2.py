e400c94312460b6f7ec774658d96a92126ca4a81
Adam Marshaev <adam@Adams-MBP.lan>
2023-12-25 22:31:57 -0800

    added climbing stairs reconstructing path with k steps optimization

--- 08 reconstructing solutions/climbing_stairs_reconstructing2.py
+++ 08 reconstructing solutions/climbing_stairs_reconstructing2.py
@@ -37,22 +37,26 @@ Space complexity: o(n)
     allocated n number of variables
 """
 
-def climbing_stairs(n:int, price:[]) -> [int]:
+def climbing_stairs(n:int, k:int, price:[]) -> [int]:
     
     ways = [0] * (n+1) # array of size k
     ways[0] = 0
-    ways[1] = price[1]
+    #ways[1] = price[1]
+    minimum = 999 # max int here
     from_array = [0] * (n+1)
 
-    for i in range(2,n+1):
-            ways[i] = min(ways[(i-1)] , ways[i-2]) + price[i]
-            
-            if ways[i-1] < ways[i-2]:
-                  from_array[i] = i-1
-            else:
-                  from_array[i] = i-2
-                  
+    for i in range(1,n+1):
+        for j in range(1,k+1): 
+            if (i-j) < 0:
+                 continue
         
+            if ways[i-j] + price[i] < minimum:
+                  from_array[i] = i-j
+                  minimum = ways[(i-j)] + price[i]
+                  ways[i] = ways[(i-j)] + price[i]
+                  
+        minimum = 999
+            
     curr = n
     reconstructed_path = []
     while(curr > 0):
@@ -62,10 +66,10 @@ def climbing_stairs(n:int, price:[]) -> [int]:
     reconstructed_path.append(0)
     reconstructed_path.reverse()
     return reconstructed_path
-
+    
 prices = [0,3,2,4,1,5,1,7,1]
-print(climbing_stairs(8, prices)) #5
+print(climbing_stairs(8,2, prices)) #5
 
 prices = [0,3,2,4]
-print(climbing_stairs(3, prices)) #6
+print(climbing_stairs(3,2, prices)) #6
 
